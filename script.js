4a8c9800aeadcb5b2cccbe337a629df17e4b7de5
Isaac <isaac.njuguna@strathmore.edu>
2024-10-26 12:23:48 +0300

    Create helper functions to update DOM

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
+function getElements(marker) {
+  let cell = document.querySelector(`[data-index="${marker}"]`);
+  return cell;
+}
+
+function updateDOM(cell, token) {
+  let img = document.createElement("img");
+  img.src = token;
+  return cell.append(img);
+}
+function createPlayer(name, token) {
+  let score = 0;
+  const incrementScore = () => score++;
+  const getScore = () => score;
+  const setMarker = (gameBoard) => {
+    // console.log(gameBoard);
+    let marker = prompt(`${name}enter a number from 1 to 9`, "");
+    if (marker === "1" && gameBoard[0][0] === null) {
+      gameBoard[0][0] = token;
+      return updateDOM(getElements(marker), token);
+    }
+    if (marker === "2" && gameBoard[0][1] === null) {
+      gameBoard[0][1] = token;
+      return updateDOM(getElements(marker), token);
+    }
+    if (marker === "3" && gameBoard[0][2] === null) {
+      gameBoard[0][2] = token;
+      return updateDOM(getElements(marker), token);
+    }
+    if (marker === "4" && gameBoard[1][0] === null) {
+      gameBoard[1][0] = token;
+      return updateDOM(getElements(marker), token);
+    }
+    if (marker === "5" && gameBoard[1][1] === null) {
+      gameBoard[1][1] = token;
+      return updateDOM(getElements(marker), token);
+    }
+    if (marker === "6" && gameBoard[1][2] === null) {
+      gameBoard[1][2] = token;
+      return updateDOM(getElements(marker), token);
+    }
+    if (marker === "7" && gameBoard[2][0] === null) {
+      gameBoard[2][0] = token;
+      return updateDOM(getElements(marker), token);
+    }
+    if (marker === "8" && gameBoard[2][1] === null) {
+      gameBoard[2][1] = token;
+      return updateDOM(getElements(marker), token);
+    }
+    if (marker === "9" && gameBoard[2][2] === null) {
+      gameBoard[2][2] = token;
+      return updateDOM(getElements(marker), token);
+    } else {
+      console.log("invalid entry");
+      return setMarker(gameBoard);
+    }
+  };
+
+  return { name, token, incrementScore, getScore, setMarker };
+}
+
 const gameLogic = (function () {
   function createGameBoard() {
     gameBoard = [
@@ -9,9 +70,11 @@ const gameLogic = (function () {
     return gameBoard;
   }
 
-  // function isGameEnded(gameBoard) {
-  //   gameBoard ?? true;
-  // }
+  // let displayDom = {
+  //   getElements: function () {
+  //     return (gridElements = document.querySelectorAll("[data-index]"));
+  //   },
+  // };
 
   function checkWinner(token, gameBoard) {
     if (
@@ -64,55 +127,13 @@ const gameLogic = (function () {
       return true;
     } else return false;
   }
-  return { createGameBoard, checkWinner };
+  return { createGameBoard, checkWinner, getElements, updateDOM };
 })();
-
-function createPlayer(name, token) {
-  let score = 0;
-  const incrementScore = () => score++;
-  const getScore = () => score;
-  const setMarker = (gameBoard) => {
-    // console.log(gameBoard);
-    let marker = prompt(`${name}enter a number from 1 to 9`, "");
-    if (marker === "1" && gameBoard[0][0] === null) {
-      return (gameBoard[0][0] = token);
-    }
-    if (marker === "2" && gameBoard[0][1] === null) {
-      return (gameBoard[0][1] = token);
-    }
-    if (marker === "3" && gameBoard[0][2] === null) {
-      return (gameBoard[0][2] = token);
-    }
-    if (marker === "4" && gameBoard[1][0] === null) {
-      return (gameBoard[1][0] = token);
-    }
-    if (marker === "5" && gameBoard[1][1] === null) {
-      return (gameBoard[1][1] = token);
-    }
-    if (marker === "6" && gameBoard[1][2] === null) {
-      return (gameBoard[1][2] = token);
-    }
-    if (marker === "7" && gameBoard[2][0] === null) {
-      return (gameBoard[2][0] = token);
-    }
-    if (marker === "8" && gameBoard[2][1] === null) {
-      return (gameBoard[2][1] = token);
-    }
-    if (marker === "9" && gameBoard[2][2] === null) {
-      return (gameBoard[2][2] = token);
-    } else {
-      console.log("invalid entry");
-      return setMarker(gameBoard);
-    }
-  };
-
-  return { name, token, incrementScore, getScore, setMarker };
-}
 
 function playGame() {
   gameBoard = gameLogic.createGameBoard();
-  let player1 = createPlayer("player1", "O");
-  let player2 = createPlayer("player2", "X");
+  let player1 = createPlayer("player1", "assets/o.png");
+  let player2 = createPlayer("player2", "assets/x.png");
   console.log([
     [1, 2, 3],
     [4, 5, 6],
@@ -136,5 +157,9 @@ function playGame() {
   }
   alert("its a tie");
 }
+// console.table(gameLogic.displayDom.getElements());
+// starts the game , commented out for testing purposes
+// playGame();
 
-// playGame(); starts the game , commented out for testing purposes
+// let player2 = createPlayer("player2", "assets/x.png");
+// console.table(player2);
